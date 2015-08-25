d6e3cbe13a5d14059e6f41ab2a3ba7fd3dc1b7d5
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2015-08-25 12:51:33 +0200

    Add service and routing files

---
+++ src/AppBundle/Controller/DestinationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Country;
+use AppBundle\Entity\Destination;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+class DestinationController extends Controller
+{
+    /**
+     * @ParamConverter("country", options={"mapping": {"slugCountry": "slug"}})
+     * @param Country $country
+     */
+    public function viewCountryAction(Country $country)
+    {
+
+    }
+
+    /**
+     * @ParamConverter("destination", options={"mapping": {"slugDestination": "slug"}})
+     * @param Destination $destination
+     */
+    public function viewDestinationAction(Destination $destination)
+    {
+
+    }
+}
