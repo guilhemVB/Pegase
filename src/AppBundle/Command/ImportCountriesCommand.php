69fa62c7436faddd210833bfb98dfc61ec67c49e
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2015-08-26 10:55:06 +0200

    add command to import countries

---
+++ src/AppBundle/Command/ImportCountriesCommand.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Country;
+use AppBundle\Repository\CountryRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Log\LoggerInterface;
+
+class ImportCountriesCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('country:import')
+            ->setDescription("Permet d'importer et mettre à jour la liste des pays")
+            ->addArgument('fileName', InputArgument::REQUIRED, 'Nom du fichier csv à importer');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $now = new \DateTime();
+        $output->writeln('<comment>Start : ' . $now->format('d-m-Y G:i:s') . ' ---</comment>');
+
+        $this->import($input, $output);
+
+        $now = new \DateTime();
+        $output->writeln('<comment>End : ' . $now->format('d-m-Y G:i:s') . ' ---</comment>');
+    }
+
+    private function import(InputInterface $input, OutputInterface $output)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        /** @var CountryRepository $countryRepository */
+        $countryRepository = $em->getRepository('AppBundle:Country');
+
+        $dataCountries = $this->getDataCountries($input, $output);
+        $nbCountries = count($dataCountries);
+        $output->writeln("<info>--- $nbCountries pays ont été trouvés dans le fichier ---</info>");
+
+        $nbToFlush = 0;
+        foreach ($dataCountries as $dataCountry) {
+            $name = $dataCountry['nom'];
+            $description = $dataCountry['description'];
+
+            $country = $countryRepository->findOneByName($name);
+            if (is_null($country)) {
+                $country = new Country();
+                $country->setName($name);
+                $output->writeln("<info>Nouveau pays '$name' ---</info>");
+            }
+            $country->setDescription($description);
+            $em->persist($country);
+            $nbToFlush++;
+            if($nbToFlush % 50 == 0) {
+                $em->flush();
+            }
+        }
+        $em->flush();
+    }
+
+    private function getDataCountries(InputInterface $input, OutputInterface $output)
+    {
+        $filenName = $input->getArgument('fileName');
+        $delimiter = ';';
+
+        if (!file_exists($filenName) || !is_readable($filenName)) {
+            $output->writeln("<error>Le fichier '$filenName' n'a pas été trouvé</error>");
+            return [];
+        }
+
+        $header = NULL;
+        $data = array();
+
+        if (($handle = fopen($filenName, 'r')) !== FALSE) {
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE) {
+                if (!$header) {
+                    $header = $row;
+                } else {
+                    $data[] = array_combine($header, $row);
+                }
+            }
+            fclose($handle);
+        }
+        return $data;
+    }
+
+}
