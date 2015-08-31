c3915916442690d00302c2851dbb49576e0c01a3
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2015-08-31 18:07:45 +0200

    add voyage route

---
+++ src/AppBundle/Controller/VoyageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/voyage")
+ */
+class VoyageController extends Controller
+{
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction()
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if(count($user->getVoyages()) == 0) {
+            // redirect to voyage creation
+        }
+    }
+
+}
