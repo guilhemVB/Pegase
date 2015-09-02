580b7feb9287693b01bd5a70104bd53b6017fff3
guilhemVB <gimli.fr@hotmail.fr>
2015-09-02 08:41:48 +0200

    improve select2

--- src/AppBundle/Resources/public/js/voyage/voyage.js
+++ src/AppBundle/Resources/public/js/voyage/voyage.js
@@ -2,7 +2,8 @@ $().ready(function () {
 
     $('#deparatureDate').datepicker();
 
-    $("#destinations").select2({
-        placeholder: "Choix d'un lieu de départ"
+    $("#destination").select2({
+        placeholder: "Choix d'un lieu de départ",
+        theme: "bootstrap"
     });
 });
