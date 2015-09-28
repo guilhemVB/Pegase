ca30b2a9edea2063a8d797ed803b9a90c69c5b4b
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2015-09-28 17:21:15 +0200

    add behat

---
+++ features/bootstrap/AppBundle/Features/Context/FeatureContext.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Features\Context;
+
+
+use Behat\Behat\Context\Context;
+use AppKernel;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+    public function __construct()
+    {
+    }
+
+    /** @BeforeScenario */
+    public function before($event)
+    {
+        // Clean database
+        $kernel = new AppKernel("test", true);
+        $kernel->boot();
+        $this->application = new Application($kernel);
+        $this->application->setAutoExit(false);
+        $this->runConsole("doctrine:schema:drop", ["--force" => true, "--full-database" => true, "--em" => "default_database_generator"]);
+        $this->runConsole("doctrine:schema:create", ["--em" => "default_database_generator"]);
+        $kernel->shutdown();
+    }
+
+    /**
+     * @param string $command
+     * @param array $options
+     * @return int
+     * @throws \Exception
+     */
+    protected function runConsole($command, $options = array())
+    {
+        $options["-e"] = "test";
+        $options["-q"] = null;
+        $options = array_merge($options, array('command' => $command));
+        return $this->application->run(new ArrayInput($options));
+    }
+}
