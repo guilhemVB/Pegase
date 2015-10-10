00b9811c39cf09081caaac45a8492bcb9dd495a6
guilhemVB <gimli.fr@hotmail.fr>
2015-10-10 10:34:55 +0200

    add remove destination modal

--- src/AppBundle/Resources/public/js/destination/destination.js
+++ src/AppBundle/Resources/public/js/destination/destination.js
@@ -7,7 +7,7 @@ $().ready(function () {
         $("#numberDays").val(1);
 
         var modal = $(this);
-        modal.find('#destinationNameModal').text(destinationName);
+        modal.find('.destinationNameModal').text(destinationName);
     });
 
     $("#addToVoyageBtn").on("click", function() {
@@ -21,7 +21,23 @@ $().ready(function () {
         }, "json");
     });
 
-    $("#btnAddToVoyage").on('click', '#removeStage', function (event) {
+    $('#removeFromVoyageModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var destinationName = button.data('destinationName');
+        var stageId = button.data('stageId');
+        var nbDays = button.data('stageNbDays');
+        var position = button.data('stagePosition');
+
+        var modal = $(this);
+        modal.find('.destinationNameModal').text(destinationName);
+        modal.find('#nbDaysRemoveFromVoyageModal').html(nbDays);
+        modal.find('#positionRemoveFromVoyageModal').html(position);
+
+        var validationBtn = modal.find("#removeFromVoyageBtn");
+        validationBtn.data('stageId', stageId);
+    });
+
+    $("#removeFromVoyageBtn").on('click', function (event) {
         var data = {
             addBtnAddToVoyage : true
         };
