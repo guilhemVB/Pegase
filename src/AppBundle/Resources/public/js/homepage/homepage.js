f8583b985cdf51b5365ea8f463d5bdc7a72afe24
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2015-10-21 13:59:11 +0200

    Remove select2 js from homepage

--- src/AppBundle/Resources/public/js/homepage/homepage.js
+++ src/AppBundle/Resources/public/js/homepage/homepage.js
@@ -1,11 +1,3 @@
 $().ready(function () {
 
-    $("#homepage .destination").select2({
-        placeholder: "Trouver une destination",
-        theme: "bootstrap"
-    });
-
-    $("#homepage .destination").on("change", function(e) {
-        document.location.href= e.target.selectedOptions[0].dataset.href;
-    });
 });
