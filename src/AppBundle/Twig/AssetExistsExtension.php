ca5deb84544de3f438ad84fe511346b7bfea2b0c
guilhemVB <gimli.fr@hotmail.fr>
2015-10-29 20:52:15 +0100

    Use photos for destinations and countries. Add default photo

---
+++ src/AppBundle/Twig/AssetExistsExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Twig;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class AssetExistsExtension extends \Twig_Extension
+{
+
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('asset_exists', array($this, 'assetExist')),
+        );
+    }
+
+    public function assetExist($path)
+    {
+        $webRoot = realpath($this->kernel->getRootDir() . '/../web');
+        $toCheck = realpath($webRoot . '/' . $path);
+
+        if (!is_file($toCheck)) {
+            return false;
+        }
+
+        // check if file is well contained in web/ directory (prevents ../ in paths)
+        if (strncmp($webRoot, $toCheck, strlen($webRoot)) !== 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getName()
+    {
+        return 'asset_exist_extension';
+    }
+}
