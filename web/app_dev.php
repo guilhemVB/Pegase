0cc7e0dec8e8610f8dfa3ef4b13af5a4142be5c2
guilhemVB <gimli.fr@hotmail.fr>
2015-11-11 10:52:40 +0100

    print ip

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,6 +10,7 @@ use Symfony\Component\Debug\Debug;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 
+var_dump(@$_SERVER['REMOTE_ADDR']);
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
