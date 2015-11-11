048c5f232f6588ebe5187f261c57b317b050f622
guilhemVB <gimli.fr@hotmail.fr>
2015-11-11 10:49:41 +0100

    test app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,6 +9,9 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+
+var_dump(@$_SERVER['REMOTE_ADDR']);
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '10.0.2.2')) || php_sapi_name() === 'cli-server')
