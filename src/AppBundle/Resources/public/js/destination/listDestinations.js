114bbb7f2bd47793e99e83712005669d8a5f7d20
guilhemVB <gimli.fr@hotmail.fr>
2015-11-12 22:48:52 +0100

    add errors on select2 destination

--- src/AppBundle/Resources/public/js/destination/listDestinations.js
+++ src/AppBundle/Resources/public/js/destination/listDestinations.js
@@ -1,15 +1,24 @@
 $().ready(function () {
 
     var $destinationSelect = $("#destinationsListView .destination");
+    var $btnSeeDestination = $("#btnSeeDestination");
+    var $formGroupDestinations = $("#formGroupDestinations");
 
     $destinationSelect.select2({
         placeholder: "Rechercher une destination",
         theme: "bootstrap"
     });
 
-    $("#btnSeeDestination").on('click', function() {
+    $btnSeeDestination.on('click', function () {
         var data = $destinationSelect.select2('data');
-        window.location.href = data.element[0].dataset.href;
+        if (data) {
+            $formGroupDestinations.addClass('has-success');
+            $formGroupDestinations.removeClass('has-error');
+            window.location.href = data.element[0].dataset.href;
+        } else {
+            $formGroupDestinations.removeClass('has-success');
+            $formGroupDestinations.addClass('has-error');
+        }
     });
 
 });
