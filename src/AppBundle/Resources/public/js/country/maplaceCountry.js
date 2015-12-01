91315d1eee5e0ab8f9911772403051aa95220b62
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2015-12-01 13:12:52 +0100

    Add share buttons in destination

--- src/AppBundle/Resources/public/js/country/maplaceCountry.js
+++ src/AppBundle/Resources/public/js/country/maplaceCountry.js
@@ -4,9 +4,7 @@ $().ready(function () {
         new Maplace({
             locations: maplaceData,
             map_div: '#gmap',
-            controls_type: 'list',
-            controls_on_map: false,
-            view_all_text: 'Tous'
+            controls_type: 'list'
         }).Load();
     });
 });
