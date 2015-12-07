fdf65adc855c1e49fb0a472803a62305924fbf3b
guilhemVB <gimli.fr@hotmail.fr>
2015-12-07 22:18:36 +0100

    Refacto homepage to make mono page

--- src/AppBundle/Controller/SiteMapController.php
+++ src/AppBundle/Controller/SiteMapController.php
@@ -22,8 +22,8 @@ class SiteMapController extends Controller
      */
     public function homepageAction()
     {
-        /** @var $em EntityManager $em */
-        $em = $this->get('doctrine')->getManager();
+//        /** @var $em EntityManager $em */
+//        $em = $this->get('doctrine')->getManager();
 
         $urls = [];
 
@@ -31,59 +31,59 @@ class SiteMapController extends Controller
                    'changefreq' => 'weekly',
                    'priority'   => '1.0'];
 
-        $urls[] = ['loc'        => $this->get('router')->generate('ideasOfTravels', [], true),
-                   'changefreq' => 'weekly',
-                   'priority'   => '0.9'];
-
-        $urls[] = ['loc'        => $this->get('router')->generate('destinationsList', [], true),
-                   'changefreq' => 'weekly',
-                   'priority'   => '0.9'];
-
-        $urls[] = ['loc'        => $this->get('router')->generate('fos_user_registration_register', [], true),
-                   'changefreq' => 'monthly',
-                   'priority'   => '0.7'];
-
-        $urls[] = ['loc'        => $this->get('router')->generate('fos_user_security_login', [], true),
-                   'changefreq' => 'monthly',
-                   'priority'   => '0.7'];
+//        $urls[] = ['loc'        => $this->get('router')->generate('ideasOfTravels', [], true),
+//                   'changefreq' => 'weekly',
+//                   'priority'   => '0.9'];
+//
+//        $urls[] = ['loc'        => $this->get('router')->generate('destinationsList', [], true),
+//                   'changefreq' => 'weekly',
+//                   'priority'   => '0.9'];
+//
+//        $urls[] = ['loc'        => $this->get('router')->generate('fos_user_registration_register', [], true),
+//                   'changefreq' => 'monthly',
+//                   'priority'   => '0.7'];
+//
+//        $urls[] = ['loc'        => $this->get('router')->generate('fos_user_security_login', [], true),
+//                   'changefreq' => 'monthly',
+//                   'priority'   => '0.7'];
 
         $urls[] = ['loc'        => $this->get('router')->generate('contact', [], true),
                    'changefreq' => 'monthly',
                    'priority'   => '0.5'];
 
-        $urls[] = ['loc'        => $this->get('router')->generate('fos_user_resetting_request', [], true),
-                   'changefreq' => 'monthly',
-                   'priority'   => '0.2'];
+//        $urls[] = ['loc'        => $this->get('router')->generate('fos_user_resetting_request', [], true),
+//                   'changefreq' => 'monthly',
+//                   'priority'   => '0.2'];
 
-        /** @var $countryRepository CountryRepository */
-        $countryRepository = $em->getRepository('AppBundle:Country');
-        $countries = $countryRepository->findCountriesWithDestinations();
-
-        /** @var Country $country */
-        foreach ($countries as $country) {
-            $urls[] = ['loc'        => $this->get('router')->generate('country', ['slug' => $country->getSlug()], true),
-                       'changefreq' => 'monthly',
-                       'priority'   => '0.7'];
-
-            $destinations = $country->getDestinations();
-            foreach ($destinations as $destination) {
-                $urls[] = ['loc'        => $this->get('router')->generate('destination', ['slug' => $destination->getSlug()], true),
-                           'changefreq' => 'monthly',
-                           'priority'   => '0.7'];
-            }
-        }
-
-
-        /** @var $typicalVoyageRepository TypicalVoyageRepository */
-        $typicalVoyageRepository = $em->getRepository('AppBundle:TypicalVoyage');
-        $typicalVoyages = $typicalVoyageRepository->findAll();
-
-        /** @var TypicalVoyage $typicalVoyage */
-        foreach ($typicalVoyages as $typicalVoyage) {
-            $urls[] = ['loc'        => $this->get('router')->generate('shareVoyage', ['token' => $typicalVoyage->getVoyage()->getToken()], true),
-                       'changefreq' => 'monthly',
-                       'priority'   => '0.7'];
-        }
+//        /** @var $countryRepository CountryRepository */
+//        $countryRepository = $em->getRepository('AppBundle:Country');
+//        $countries = $countryRepository->findCountriesWithDestinations();
+//
+//        /** @var Country $country */
+//        foreach ($countries as $country) {
+//            $urls[] = ['loc'        => $this->get('router')->generate('country', ['slug' => $country->getSlug()], true),
+//                       'changefreq' => 'monthly',
+//                       'priority'   => '0.7'];
+//
+//            $destinations = $country->getDestinations();
+//            foreach ($destinations as $destination) {
+//                $urls[] = ['loc'        => $this->get('router')->generate('destination', ['slug' => $destination->getSlug()], true),
+//                           'changefreq' => 'monthly',
+//                           'priority'   => '0.7'];
+//            }
+//        }
+//
+//
+//        /** @var $typicalVoyageRepository TypicalVoyageRepository */
+//        $typicalVoyageRepository = $em->getRepository('AppBundle:TypicalVoyage');
+//        $typicalVoyages = $typicalVoyageRepository->findAll();
+//
+//        /** @var TypicalVoyage $typicalVoyage */
+//        foreach ($typicalVoyages as $typicalVoyage) {
+//            $urls[] = ['loc'        => $this->get('router')->generate('shareVoyage', ['token' => $typicalVoyage->getVoyage()->getToken()], true),
+//                       'changefreq' => 'monthly',
+//                       'priority'   => '0.7'];
+//        }
 
         return $this->render('AppBundle:SiteMap:sitemap.xml.twig', ['urls' => $urls]);
     }
