3e0c746a17ae975866a4d919a7d9c291c81f20d8
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2015-12-14 14:00:08 +0100

    Add facebook button

--- src/AppBundle/Resources/public/js/homepage/homepage.js
+++ src/AppBundle/Resources/public/js/homepage/homepage.js
@@ -1,5 +1,24 @@
 $().ready(function () {
 
+    // ---------- FACEBOOK ----------
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1510306902598039',
+            xfbml      : true,
+            version    : 'v2.5'
+        });
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+
+    // ---------- TWITTER ----------
     !function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
         if (!d.getElementById(id)) {
