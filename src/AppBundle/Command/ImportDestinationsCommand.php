0dc309c79d20b04d8ffb59000fd0343fd491af67
guilhemVB <gimli.fr@hotmail.fr>
2015-12-24 15:33:58 +0100

    Increase the max length of the description Add Saint Petersbourg banner

--- src/AppBundle/Command/ImportDestinationsCommand.php
+++ src/AppBundle/Command/ImportDestinationsCommand.php
@@ -16,7 +16,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ImportDestinationsCommand extends ContainerAwareCommand
 {
 
-    const MAX_SIZE_DESCRIPTION_LENGTH = 950;
+    const MAX_SIZE_DESCRIPTION_LENGTH = 1020;
 
     /** @var  AssetExistsExtension */
     private $assetExistsExtension;
