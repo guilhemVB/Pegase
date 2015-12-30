01655a2b32e470c16a5e312bcfd9fc2ddd4b6f58
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2015-12-30 16:49:07 +0100

    Add tooltip to explain accommodation price

--- src/AppBundle/Resources/public/js/destination/destination.js
+++ src/AppBundle/Resources/public/js/destination/destination.js
@@ -1,51 +1,55 @@
 $().ready(function () {
 
-    $('#addToVoyageModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var destinationName = button.data('destinationName');
-
-        $("#numberDays").val(7);
-
-        var modal = $(this);
-        modal.find('.destinationNameModal').text(destinationName);
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
     });
 
-    $("#addToVoyageBtn").on("click", function() {
-        var data = {
-            nbDays : $("#numberDays").val(),
-            addBtnAddToVoyage : true
-        };
-        $.post(addStageUrl, data, function (response) {
-            $.updateNavBarInfos();
-            $("#btnAddToVoyage").html(response.btnAddToVoyage);
-        }, "json");
-    });
-
-    $('#removeFromVoyageModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var destinationName = button.data('destinationName');
-        var stageId = button.data('stageId');
-        var nbDays = button.data('stageNbDays');
-        var position = button.data('stagePosition');
-
-        var modal = $(this);
-        modal.find('.destinationNameModal').text(destinationName);
-        modal.find('#nbDaysRemoveFromVoyageModal').html(nbDays);
-        modal.find('#positionRemoveFromVoyageModal').html(position);
-
-        var validationBtn = modal.find("#removeFromVoyageBtn");
-        validationBtn.data('stageId', stageId);
-    });
-
-    $("#removeFromVoyageBtn").on('click', function (event) {
-        var data = {
-            addBtnAddToVoyage : true
-        };
-        var url = removeStageUrl.replace("0", $(this).data("stageId"));
-        $.post(url, data, function (response) {
-            $.updateNavBarInfos();
-            $("#btnAddToVoyage").html(response.btnAddToVoyage);
-        }, "json");
-    });
+    //$('#addToVoyageModal').on('show.bs.modal', function (event) {
+    //    var button = $(event.relatedTarget);
+    //    var destinationName = button.data('destinationName');
+    //
+    //    $("#numberDays").val(7);
+    //
+    //    var modal = $(this);
+    //    modal.find('.destinationNameModal').text(destinationName);
+    //});
+    //
+    //$("#addToVoyageBtn").on("click", function() {
+    //    var data = {
+    //        nbDays : $("#numberDays").val(),
+    //        addBtnAddToVoyage : true
+    //    };
+    //    $.post(addStageUrl, data, function (response) {
+    //        $.updateNavBarInfos();
+    //        $("#btnAddToVoyage").html(response.btnAddToVoyage);
+    //    }, "json");
+    //});
+    //
+    //$('#removeFromVoyageModal').on('show.bs.modal', function (event) {
+    //    var button = $(event.relatedTarget);
+    //    var destinationName = button.data('destinationName');
+    //    var stageId = button.data('stageId');
+    //    var nbDays = button.data('stageNbDays');
+    //    var position = button.data('stagePosition');
+    //
+    //    var modal = $(this);
+    //    modal.find('.destinationNameModal').text(destinationName);
+    //    modal.find('#nbDaysRemoveFromVoyageModal').html(nbDays);
+    //    modal.find('#positionRemoveFromVoyageModal').html(position);
+    //
+    //    var validationBtn = modal.find("#removeFromVoyageBtn");
+    //    validationBtn.data('stageId', stageId);
+    //});
+    //
+    //$("#removeFromVoyageBtn").on('click', function (event) {
+    //    var data = {
+    //        addBtnAddToVoyage : true
+    //    };
+    //    var url = removeStageUrl.replace("0", $(this).data("stageId"));
+    //    $.post(url, data, function (response) {
+    //        $.updateNavBarInfos();
+    //        $("#btnAddToVoyage").html(response.btnAddToVoyage);
+    //    }, "json");
+    //});
 
 });
