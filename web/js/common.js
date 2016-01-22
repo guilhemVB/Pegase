265c45260760d28902d35d2fdff641ddf4a0c0c4
Guilhem Bourgoin <guilhem.bourgoin@reputationvip.com>
2016-01-22 13:43:24 +0100

    Check MaPlace exist before use it

--- web/js/common.js
+++ web/js/common.js
@@ -1,11 +1,13 @@
 $().ready(function () {
 
     $(document).ready(function () {
-        new Maplace({
-            locations: maplaceData,
-            map_div: '#gmap',
-            controls_on_map: false
-        }).Load();
+        if (typeof(Maplace) !== 'undefined') {
+            new Maplace({
+                locations: maplaceData,
+                map_div: '#gmap',
+                controls_on_map: false
+            }).Load();
+        }
     });
 
     $(function () {
