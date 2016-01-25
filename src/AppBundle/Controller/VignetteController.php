1647dc58e46df46466a4e6cfb56774b53f5106d5
guilhemVB <gimli.fr@hotmail.fr>
2016-01-25 20:22:08 +0100

    Externalisation render vignette by slug

---
+++ src/AppBundle/Controller/VignetteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Destination;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/vignette")
+ */
+class VignetteController extends Controller
+{
+
+    /**
+     * @Route("/{slug}", name="vignette")
+     * @param Destination $destination
+     * @return Response
+     */
+    public function getAction(Destination $destination)
+    {
+        return $this->render('AppBundle:Vignette:destinationLarge.html.twig',
+            [
+                'destination'    => $destination,
+            ]);
+    }
+
+}
