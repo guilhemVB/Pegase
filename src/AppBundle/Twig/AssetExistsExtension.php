ed57d511f763aa878bfedde9224312d13a27d627
guilhemVB <gimli.fr@hotmail.fr>
2016-03-06 19:58:21 +0100

    Remove duplicate entry in twig extension

--- src/AppBundle/Twig/AssetExistsExtension.php
+++ src/AppBundle/Twig/AssetExistsExtension.php
@@ -16,6 +16,7 @@ class AssetExistsExtension extends \Twig_Extension
     {
         return array(
             new \Twig_SimpleFilter('asset_exists', array($this, 'assetExist')),
+            new \Twig_SimpleFilter('journeyTime', array($this, 'journeyTimePrinter')),
         );
     }
 
@@ -38,6 +39,24 @@ class AssetExistsExtension extends \Twig_Extension
         }
 
         return true;
+    }
+
+    public function journeyTimePrinter($timeInMinutes)
+    {
+        $hours = floor($timeInMinutes / 60);
+        $minutes = $timeInMinutes % 60;
+
+        if ($minutes == 0) {
+            $minutes = '00';
+        } elseif ($minutes < 10) {
+            $minutes = '0' . $minutes;
+        }
+
+        if ($hours > 0) {
+            return sprintf("%sh%s", $hours, $minutes);
+        } else {
+            return sprintf("%smin", $minutes);
+        }
     }
 
     public function getName()
