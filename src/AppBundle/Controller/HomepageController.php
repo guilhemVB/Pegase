bfe3b6a8d5d8a7561f4016a76d86aa85b0b40881
guilhemVB <gimli.fr@hotmail.fr>
2016-03-21 22:17:18 +0100

    Print last typical voyage in homepage

--- src/AppBundle/Controller/HomepageController.php
+++ src/AppBundle/Controller/HomepageController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Destination;
 use AppBundle\Repository\DestinationRepository;
 use AppBundle\Service\MaplaceMarkerBuilder;
+use CalculatorBundle\Repository\VoyageRepository;
+use CalculatorBundle\Service\Stats\StatCalculators\StatCalculatorDestinations;
+use CalculatorBundle\Service\Stats\StatCalculators\StatCalculatorNumberDays;
+use CalculatorBundle\Service\Stats\StatCalculators\StatCalculatorPrices;
+use CalculatorBundle\Service\Stats\VoyageStats;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -24,6 +29,9 @@ class HomepageController extends Controller
         /** @var $destinationRepository DestinationRepository */
         $destinationRepository = $em->getRepository('AppBundle:Destination');
 
+        /** @var $voyageRepository VoyageRepository */
+        $voyageRepository = $em->getRepository('CalculatorBundle:Voyage');
+
         /** @var MaplaceMarkerBuilder $maplaceMarkerBuilder */
         $maplaceMarkerBuilder = $this->get('maplace_marker_builder');
 
@@ -34,10 +42,30 @@ class HomepageController extends Controller
 
         $lastDestinationsCreated = $destinationRepository->findLastCompleteDestinations();
 
+        $voyages = $voyageRepository->findTypicalVoyages($this->getParameter('typical_voyage_user_id'), 3);;
+
+        /** @var VoyageStats $voyageStats */
+        $voyageStats = $this->get('voyage_stats');
+
+        $lastVoyagesWithStats = [];
+        foreach ($voyages as $voyage) {
+            $stages = $voyage->getStages();
+            $stats = $voyageStats->calculate($voyage, $stages, [
+                new StatCalculatorDestinations(3),
+                new StatCalculatorPrices(),
+                new StatCalculatorNumberDays(),
+            ]);
+            $lastVoyagesWithStats[] = [
+                'voyage' => $voyage,
+                'stats'  => $stats,
+            ];
+        }
+
         return $this->render('AppBundle:Homepage:homepage.html.twig',
             [
                 'maplaceData'             => json_encode($maplaceData),
                 'lastDestinationsCreated' => $lastDestinationsCreated,
+                'lastVoyagesWithStats'    => $lastVoyagesWithStats,
             ]);
     }
 }
--- src/CalculatorBundle/Repository/VoyageRepository.php
+++ src/CalculatorBundle/Repository/VoyageRepository.php
@@ -14,16 +14,24 @@ class VoyageRepository extends EntityRepository
 {
     /**
      * @param string $userId
+     * @param int|null $limit
      * @return Voyage[]
      */
-    public function findTypicalVoyages($userId)
+    public function findTypicalVoyages($userId, $limit = null)
     {
         $qb = $this->createQueryBuilder('voyage')
             ->select('voyage')
             ->join('voyage.user', 'user')
             ->join('voyage.stages', 'stages')
             ->where('user.id = :userId')
+            ->groupBy('voyage.id')
             ->setParameter('userId', $userId);
+
+        if ($limit) {
+            $qb->setMaxResults($limit);
+            $qb->orderBy('voyage.id', 'DESC');
+        }
+
 
         return $qb->getQuery()->getResult();
     }
